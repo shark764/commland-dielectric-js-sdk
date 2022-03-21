60900a5ea7e51629fba1c28dc0fca89e84222324
Farid Hernandez <farid.hdz.64@gmail.com>
2022-03-21 10:42:03 -0600

    test: Exposing sdk to window global

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as plugins from './capacitor';
 
-export default {
+const capacitorSdk = {
   plugins,
   multiply: (a: number, b: number) => a * b,
   capitalize: (s: string): string => s.charAt(0).toUpperCase() + s.slice(1),
@@ -9,3 +9,14 @@ export default {
       .replace(/([A-Z])/g, ' $1')
       .replace(/^./, (str: string) => str.toUpperCase()),
 };
+declare global {
+  interface Window {
+    CapacitorSdk: typeof capacitorSdk;
+  }
+}
+
+window.CapacitorSdk = capacitorSdk;
+// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+window.CapacitorSdk = window.CapacitorSdk || {};
+
+export default capacitorSdk;
