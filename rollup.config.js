5289f56e0322757df46c5c5aa774cde96047e86a
Farid Hernandez <farid.hdz.64@gmail.com>
2022-03-25 09:28:58 -0600

    refactor: Moving to rollup to generate builds

---
+++ rollup.config.js
@@ -0,0 +1,83 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json';
+
+const input = ['src/index.ts'];
+const pkgName = pkg.name.split('/')[1].replace(/^@.*\//, '');
+const author = pkg.author;
+const banner = `
+  /**
+   * @license
+   * author: ${author}
+   * ${pkgName}.js v${pkg.version}
+   * Released under the ${pkg.license} license.
+   */
+`;
+
+export default [
+  {
+    // UMD
+    input,
+    output: {
+      file: `lib/${pkgName}.min.js`,
+      format: 'umd',
+      name: 'commlandMobile', // this is the name of the global object
+      esModule: false,
+      exports: 'named',
+      sourcemap: true,
+      inlineDynamicImports: true,
+      banner,
+    },
+    plugins: [
+      nodeResolve(),
+      babel({
+        babelHelpers: 'bundled',
+      }),
+      terser(),
+      commonjs({
+        include: 'node_modules/**',
+        exclude: '**/*.css',
+        extensions: ['.js', '.ts'],
+      }),
+      typescript(),
+    ],
+  },
+  // ESM and CJS
+  {
+    input,
+    output: [
+      {
+        dir: 'lib/esm',
+        format: 'esm',
+        exports: 'named',
+        sourcemap: true,
+        banner,
+      },
+      {
+        dir: 'lib/cjs',
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true,
+        banner,
+      },
+    ],
+    plugins: [
+      nodeResolve(),
+      babel({
+        babelHelpers: 'bundled',
+      }),
+      commonjs({
+        extensions: ['.js', '.ts'],
+      }),
+      typescript(),
+    ],
+    external: [
+      '@capacitor/core',
+      '@capacitor/browser',
+      '@capacitor/geolocation',
+    ],
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,2 @@
-import * as plugins from './capacitor';
-
-const capacitorSdk = {
-  plugins,
-  multiply: (a: number, b: number) => a * b,
-  capitalize: (s: string): string => s.charAt(0).toUpperCase() + s.slice(1),
-  humanize: (s: string): string =>
-    s
-      .replace(/([A-Z])/g, ' $1')
-      .replace(/^./, (str: string) => str.toUpperCase()),
-};
-declare global {
-  interface Window {
-    CapacitorSdk: typeof capacitorSdk;
-  }
-}
-
-window.CapacitorSdk = capacitorSdk;
-// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-window.CapacitorSdk = window.CapacitorSdk || {};
-
-export default capacitorSdk;
+export { default as CapacitorSdk } from './capacitor';
+export { default as basic } from './basic';
